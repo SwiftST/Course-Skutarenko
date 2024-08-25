eb9a12483343a795bb0ad6c60c5f38f436f4185a
SwiftST <pavel.zubrilov@icloud.com>
2024-08-25 15:11:00 +0500

    repeat homework

--- SwiftHWSkutarenko.playground/Pages/HW 13 Class and Struct.xcplaygroundpage/Contents.swift
+++ SwiftHWSkutarenko.playground/Pages/HW 13 Class and Struct.xcplaygroundpage/Contents.swift
@@ -6,53 +6,47 @@ import Foundation
 
 // 1. Создать структуры Student у которого будет имя, фамилия, год рождения и средний бал оценок. Сделать несколько экземпляров положить их в массив с именем журнал.
 
-struct Student {
+struct StudentSt {
     var name: String
     var surname: String
     var yearOfBirth: Int
     var averageScore: Double
 }
-var magazineStruct: [Student] = []
-
-var stStruct1 = Student(name: "Petr", surname: "Petrov", yearOfBirth: 1992, averageScore: 3.4)
-var stStruct2 = Student(name: "Ivan", surname: "Ivanov", yearOfBirth: 1988, averageScore: 4.1)
-var stStruct3 = Student(name: "Roman", surname: "Ivanov", yearOfBirth: 1991, averageScore: 4.2)
-
-magazineStruct = [stStruct1, stStruct2, stStruct3]
+var studentOneSt = StudentSt(name: "Leo", surname: "Norman", yearOfBirth: 1999, averageScore: 3.2)
+var studentTwoSt = StudentSt(name: "Fil", surname: "Norman", yearOfBirth: 2001, averageScore: 4.1)
+var studentThreeSt = StudentSt(name: "Bill", surname: "Colins", yearOfBirth: 1978, averageScore: 3)
+var studentSt = [studentOneSt, studentTwoSt, studentThreeSt]
 
 // 2. Создать функцию принимающую массив и выводит в консоль данные студента перед каждым студентом ставить номер по списку (начиная с 1).
 
-func printStudents(for students: [Student]) {
+func printStudentsSt(from students: [StudentSt]) {
     for (i, student) in students.enumerated() {
-        print("\(i + 1). \(student.name) \(student.surname), year of birth: \(student.yearOfBirth), average score: \(student.averageScore)")
+        print("\(i + 1). Name: \(student.name), Surname: \(student.surname), birth year: \(student.yearOfBirth), average score: \(student.averageScore)")
     }
     print("")
 }
-printStudents(for: magazineStruct)
+printStudentsSt(from: studentSt)
+
 
 // 3. Используя функцию sorted(by:) отсортировать массив по среднему балу по убыванию и распечатать весь массив.
 
-magazineStruct = magazineStruct.sorted { $0.averageScore > $1.averageScore
-}
-printStudents(for: magazineStruct)
+printStudentsSt(from: studentSt.sorted { $0.averageScore > $1.averageScore })
 
 // 4. Отсортировать массив по возрастанию фамилии и если фамилии одинаковы тогда сортировать по имени, распечатать еще раз.
 
-magazineStruct = magazineStruct.sorted { one, two in
-    one.surname == two.surname ? one.name < two.name : one.surname < two.surname
-}
-printStudents(for: magazineStruct)
-
+printStudentsSt(from: studentSt.sorted {
+    $0.surname == $1.surname ? $0.name < $1.name : $0.surname < $1.surname
+})
 
 // 5. Создайте переменную и присвойте ей ваш существующий массив. Измените в нем данные всех студентов. Изменится ли первый массив? Распечатайте оба массива.
 
-var someMagazine = magazineStruct
-someMagazine[0].name = "Julia"
-someMagazine[1].yearOfBirth = 999
-someMagazine[2].averageScore = 1000
+var someStudentSt = studentSt
+someStudentSt[0].name = "Jim"
+someStudentSt[1].name = "Julia"
+someStudentSt[2].name = "Ron"
 
-printStudents(for: someMagazine)
-printStudents(for: magazineStruct)
+printStudentsSt(from: someStudentSt)
+printStudentsSt(from: studentSt)
 
 // Исходный массив не изменится, так как мы имеем дело c Value type, когда мы создаем новый массив студентов, присваивая исходный массив создается копия, изменяя которую мы никак не влияем на исходный массив
 
@@ -60,68 +54,58 @@ printStudents(for: magazineStruct)
 
 // 1. Создать структуры Student у которого будет имя, фамилия, год рождения и средний бал оценок. Сделать несколько экземпляров положить их в массив с именем журнал.
 
-class StudentClass {
+class StudentCl {
     var name: String
     var surname: String
-    var yersOfBirth: Int
+    var birthOfYear: Int
     var averageScore: Double
     
-    init(_ name: String, _ surname: String, _ years: Int, _ score: Double) {
+    init(_ name: String, _ surname: String, _ birthOfYear: Int, _ averageScore: Double) {
         self.name = name
         self.surname = surname
-        yersOfBirth = years
-        averageScore = score
+        self.birthOfYear = birthOfYear
+        self.averageScore = averageScore
     }
 }
 
-let stClass1 = StudentClass("Vasiliy", "Petrov", 1988, 2.6)
-let stClass2 = StudentClass("Genadiy", "Petrov", 1976, 4.6)
-let stClass3 = StudentClass("Viktoria", "Secret", 1989, 4.1)
-
-var students = [stClass1, stClass2, stClass3]
+var studentOneCl = StudentCl("Ron", "Uizly", 1991, 3.1)
+var studentTwoCl = StudentCl("Mihail", "Cat", 1967, 4.2)
+var studentThreeCl = StudentCl("Bob", "Cat", 1989, 2.3)
+var studentsCl = [studentOneCl, studentTwoCl, studentThreeCl]
 
 // 2. Создать функцию принимающую массив и выводит в консоль данные студента перед каждым студентом ставить номер по списку (начиная с 1).
 
-func printStudents(_ students: [StudentClass]) {
+func printStudentsCl(_ students: [StudentCl]) {
     for (i, student) in students.enumerated() {
-        print("\(i + 1). \(student.name) \(student.surname), year of birth: \(student.yersOfBirth), average score: \(student.averageScore)")
+        print("\(i + 1). Name: \(student.name), Surname: \(student.surname), Year birth: \(student.birthOfYear), Average score: \(student.averageScore)")
     }
     print("")
 }
-printStudents(students)
+printStudentsCl(studentsCl)
 
 // 3. Используя функцию sorted(by:) отсортировать массив по среднему балу по убыванию и распечатать весь массив.
 
-students = students.sorted { $0.averageScore < $1.averageScore }
-printStudents(students)
+printStudentsCl(studentsCl.sorted { $0.averageScore > $1.averageScore })
 
 // 4. Отсортировать массив по возрастанию фамилии и если фамилии одинаковы тогда сортировать по имени, распечатать еще раз.
 
-students = students.sorted { one, two in
-    one.surname == two.surname ? one.name < two.name : one.surname < two.surname
-}
-printStudents(students)
+printStudentsCl(studentsCl.sorted { $0.surname == $1.surname ? $0.name < $1.name : $0.surname < $1.surname })
 
 // 5. Создайте переменную и присвойте ей ваш существующий массив. Измените в нем данные всех студентов. Изменится ли первый массив? Распечатайте оба массива.
 
 // Данные не изменятся, так как мы создадим ссылку на уже существующий объект, изменяя какой либо экземпляр результирующего массива будут изменяться данные как в исходном массиве, так и в самих экземплярах класса входящих в состав массива
 
-var someStudents = students
-someStudents[0].name = "Friday"
-someStudents[1].surname = "Wednesday"
-someStudents[2].averageScore = 9999
+var someStudentsCl = studentsCl
+someStudentsCl[0].name = "Friday"
+someStudentsCl[1].surname = "Wednesday"
+someStudentsCl[2].averageScore = 9999
 
-printStudents(someStudents)
-printStudents(students)
+printStudentsCl(someStudentsCl)
+printStudentsCl(studentsCl)
 
-someStudents[0] === students[0]
-someStudents[0] === stClass2
-stClass1 === someStudents[1]
-stClass3 === students[2]
-
-stClass2.name
-stClass1.surname
-stClass3.averageScore
+someStudentsCl[0] === studentsCl[0]
+someStudentsCl[0] === studentOneCl
+studentTwoCl === someStudentsCl[1]
 
 
 
